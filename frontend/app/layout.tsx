e4cec32f372da6b1fd2dd68ca6db4f0bca365e5b
hrnns-ti <nassjourney@gmail.com>
2026-08-17 18:33:09 +0700

    add: init frontend

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Barlow } from "next/font/google";
+import "./globals.css";
+
+const barlow = Barlow({
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-barlow",
+  subsets: ["latin"]
+})
+
+export const metadata: Metadata = {
+  title: "Kiroku",
+  description: "Your Personal Anime Library",
+};
+
+export default function RootLayout({ children }: LayoutProps<"/">) {
+  return (
+    <html
+      lang="en"
+      className={`${barlow.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facilis praesentium quisquam labore rem, consequuntur voluptates quia cum atque esse ab rerum distinctio fugiat nemo! Sunt maxime suscipit earum quaerat.</p>
+    </main>
+  );
+}
